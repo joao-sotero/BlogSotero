c6ccfe61a47b161401438d1f97d653ea434e03e4
joao-sotero <vitor99jjunior@gmail.com>
2021-05-24 07:48:40 -0300

    correct commit

---
+++ front-end/blog/src/app/feed/feed.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
+import {Post} from '../model/Post';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+
+  listPost!: Post[];
+
+  constructor(private postService:PostService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  findPosts() {
+    this.postService.getPosts().subscribe((data: Post[]) => {
+      this.listPost = data;
+    })
+  }
+
+}
